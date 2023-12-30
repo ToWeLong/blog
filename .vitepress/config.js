61a37ca3b3a0fb33c3bc0c326f23862147c709e4
towelong <towelong@qq.com>
2023-12-30 17:28:24 +0800

    feat: add website statistic

--- .vitepress/config.js
+++ .vitepress/config.js
@@ -1,5 +1,5 @@
 import { defineConfig } from 'vitepress'
-import footnote_plugin from "markdown-it-footnote";
+import footnote_plugin from 'markdown-it-footnote'
 import { getPosts } from './theme/serverUtils'
 
 //每页的文章数量
@@ -7,10 +7,21 @@ const pageSize = 10
 
 export default defineConfig({
     title: 'welong',
+    titleTemplate: ':title - WeLong的博客',
     base: '/blog/',
     cacheDir: './node_modules/vitepress_cache',
     description: 'towelong,博客,blog,golang学习,Python学习',
     ignoreDeadLinks: true,
+    head: [
+        [
+            'script',
+            {
+                async: '',
+                src: 'https://uwei.zeabur.app/script.js',
+                'data-website-id': 'b19e7f1a-f9af-473a-ad36-7153787a1244'
+            }
+        ]
+    ],
     themeConfig: {
         posts: await getPosts(pageSize),
         website: 'https://towelong.github.io/blog', //copyright link
@@ -28,7 +39,7 @@ export default defineConfig({
             // { text: 'Airene', link: 'http://airene.net' }  -- External link test
         ],
         search: {
-            provider: 'local',
+            provider: 'local'
         },
         //outline:[2,3],
         outlineTitle: '文章摘要',
@@ -46,4 +57,3 @@ export default defineConfig({
         }
     }
 })
-
